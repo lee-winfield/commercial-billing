d31b2aee446ac9287e4497263e927a0e5201c823
lee-winfield <winfieldlee01@gmail.com>
2019-02-17 08:32:21 -0600

    refactored BillingPage to use hooks

--- src/components/BillingPage.tsx
+++ src/components/BillingPage.tsx
@@ -6,8 +6,8 @@ import getNextInvoiceNum from '../helpers/getNextInvoiceNum'
 import getRecipients from '../helpers/getRecipients'
 import getSources from '../helpers/getSources'
 import { Prompt } from 'react-router-dom'
-import getBills from 'src/helpers/getBills';
-
+import getBills from 'src/helpers/getBills'
+const { useEffect, useState } = React
 
 export interface RecipientInfo {
   address1: string
@@ -22,70 +22,53 @@ export interface Bill {
   location: string
 }
 
-interface BillingPageState {
-  bills: Bill[] | null
-  isBillingModalOpen: boolean
-  recipients: object[]
-  sources: object[]
-}
-class BillingPage extends React.Component<any, BillingPageState> {
-  constructor(props: any) {
-    super(props)
+const BillingPage: React.SFC<any> = (props: any) => {
+  const [ bills, setBills ] = useState(null)
+  const [ recipients, setRecipients ] = useState([])
+  const [ sources, setSources ] = useState([])
+  const [ isBillingModalOpen, setIsBillingModalOpen ] = useState(false)
 
-    this.state = {
-      bills: null,
-      isBillingModalOpen: false,
-      recipients: [],
-      sources: [],
-    }
-  }
-
-  async componentDidMount() {
+  async function initialize() {
     const bills = getBills()
     const recipients = getRecipients()
     const sources = getSources()
-    this.setState({ bills: await bills, recipients: await recipients, sources: await sources })
+
+    setBills(await bills)
+    setRecipients(await recipients)
+    setSources(await sources)
+
     window.onbeforeunload = () => "Are you certain that you want to leave? Work may be lost"  
   }
 
-  componentDidUpdate() {
-    window.onbeforeunload = () => "Are you certain that you want to leave? Work may be lost"
-  }
+  useEffect( () => {
+    initialize()
+  }, [])
 
-  addBill = bill => {
-    this.setState({bills: [bill, ...this.state.bills]})
-  }
+  const closeModal = () => setIsBillingModalOpen(false)
+  const openModal = () => setIsBillingModalOpen(true)
+  const addBill = bill => setBills([bill, ...bills])
+  const nextInvoiceNum = getNextInvoiceNum(bills)
 
-  public render() {
-    const { bills, isBillingModalOpen, recipients, sources } = this.state
-    const openModal = () => this.setState({ isBillingModalOpen: true })
-    const closeModal = () => this.setState({ isBillingModalOpen: false })
-    const nextInvoiceNum = getNextInvoiceNum(bills)
-    const addBill = this.addBill
-    
-    return (
-      <>
-        <Prompt
-          when={true}
-          message={location =>
-            `Are you sure you want to go to ${location.pathname}`
-          }
-        />
-        <Cards bills={bills} />
-        <div className='fab' onClick={openModal} >
-          +
-        </div>
-        <BillingModal
-          isBillingModalOpen={isBillingModalOpen}
-          closeModal={closeModal}
-          nextInvoiceNum={nextInvoiceNum}
-          recipients={recipients}
-          sources={sources}
-          addBill={addBill}
-        />
-      </>
-    )
-  }
+  return (
+    <>
+      <Prompt
+        when={true}
+        message={location => `Are you sure you want to go to ${location.pathname}`}
+      />
+      <Cards bills={bills} />
+      <div className='fab' onClick={openModal} >
+        +
+      </div>
+      <BillingModal
+        isBillingModalOpen={isBillingModalOpen}
+        closeModal={closeModal}
+        nextInvoiceNum={nextInvoiceNum}
+        recipients={recipients}
+        sources={sources}
+        addBill={addBill}
+      />
+    </>
+  )
 }
 
 export default BillingPage;
