57f3b6bb903a56300d7ba40d473c2a2cace2fa2c
lee-winfield <winfieldlee01@gmail.com>
2019-02-17 09:04:31 -0600

    added useState hook to Cards

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -4,82 +4,64 @@ import { Bill } from './BillingPage'
 import { Panel } from 'react-bootstrap'
 import { Modal, Button } from 'react-bootstrap'
 import { TablePreview } from './Confirmation'
+const { useState } = React
 
 
 interface CardsProps {
   bills: Bill[]
 }
 
+const Card: React.SFC<any> = (props) => {
+  const [ modalOpen, setModalOpen ] = useState(false)
+  const openModal = () => setModalOpen(true)
+  const closeModal = () => setModalOpen(false)
 
-class Card extends React.Component<any, any> {
-  constructor(props) {
-    super(props)
-    this.state = {
-      modalOpen: false,
-    }
-  }
-
-  openModal = () => {
-    this.setState({modalOpen: true})
-  }
-  closeModal = () => {
-    this.setState({modalOpen: false})
-  }
-
-  render() {
-    const { bill } = this.props
-    const { invoiceNum, location, recipientInfo, createdOn } = bill
-    const handleClick = () => {
-      this.openModal()
-    }
-    const closeModal = this.closeModal
-    
-    
-    
-    return (
-      <>
-        <div className='card' onClick={handleClick}>
-          <h3 className={'card-header'}>
-            <div>Invoice: {invoiceNum}</div>
-          </h3>
-          <div className='card-info'>
-            <strong>
-              {'Recipient: '}
-            </strong>
-            {recipientInfo.name}
-          </div>
-          <div className='card-info'>
-            <strong>
-              {'Date Created: '}
-            </strong>
-            {createdOn ? new Date(createdOn).toDateString() : 'N/A'}
-          </div>
-          <div className='download'>
-            <a href={location} onClick={e => {e.stopPropagation()}}>Download</a>
-          </div>
+  const { bill } = props
+  const { invoiceNum, location, recipientInfo, createdOn } = bill
+  const handleClick = () => openModal()
+  
+  return (
+    <>
+      <div className='card' onClick={handleClick}>
+        <h3 className={'card-header'}>
+          <div>Invoice: {invoiceNum}</div>
+        </h3>
+        <div className='card-info'>
+          <strong>
+            {'Recipient: '}
+          </strong>
+          {recipientInfo.name}
         </div>
-        <Modal show={this.state.modalOpen} dialogComponentClass='billing-modal'>
-          <Modal.Dialog
-              dialogClassName='billing-modal'
-          >
-            <Modal.Header>
-              <Modal.Title>Bill Information</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <TablePreview
-                document={bill}
-              />
-            </Modal.Body>
-            <Modal.Footer>
-              <Button onClick={closeModal}>Close</Button>
-            </Modal.Footer>
-          </Modal.Dialog>
-        </Modal>
-      </>
-    )
-  }
+        <div className='card-info'>
+          <strong>
+            {'Date Created: '}
+          </strong>
+          {createdOn ? new Date(createdOn).toDateString() : 'N/A'}
+        </div>
+        <div className='download'>
+          <a href={location} onClick={e => {e.stopPropagation()}}>Download</a>
+        </div>
+      </div>
+      <Modal show={modalOpen} dialogComponentClass='billing-modal'>
+        <Modal.Dialog
+            dialogClassName='billing-modal'
+        >
+          <Modal.Header>
+            <Modal.Title>Bill Information</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <TablePreview
+              document={bill}
+            />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={closeModal}>Close</Button>
+          </Modal.Footer>
+        </Modal.Dialog>
+      </Modal>
+    </>
+  )
 }
-
 
 const Cards: React.SFC<CardsProps> = ({ bills }) => (
   <Panel className='card-panel'>
